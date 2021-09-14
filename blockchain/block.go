f7b00a21dbc4f3bd655c5b1e09192f0c8e40c15f
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-09-14 18:53:04 -0300

    feat: add the POW

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,39 +1,33 @@
-package bchain
-
-import (
-	"bytes"
-	"crypto/sha256"
-)
+package blockchain
 
 type BlockChain struct {
-	blocks []*Block
+	Blocks []*Block
 }
 
 type Block struct {
 	Hash 	 []byte
 	Data	 []byte
 	PrevHash []byte
-}
-
-func (b *Block) DeriveHash() {
-	// Gerar um novo hash baseado nas infos da Data e PrevHash
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
+	Nonce    int
 }
 
 func CreateBlock(data string, prevHash []byte) *Block {
 	// Criar a estrutura do novo bloco
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
+	block := &Block{[]byte{}, []byte(data), prevHash, 0}
+	pow := NewProof(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
 
 func (chain *BlockChain) AddBlock(data string) {
 	// Adicionar o block na corrente
-	prevBlock := chain.blocks[len(chain.blocks)-1]
+	prevBlock := chain.Blocks[len(chain.Blocks)-1]
 	new := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
+	chain.Blocks = append(chain.Blocks, new)
 }
 
 func Genesis() *Block {
--- main.go
+++ main.go
@@ -2,21 +2,27 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
-	"github.com/rafaelcr1dev/Golang-Blockchain/bchain"
+	"github.com/rafaelcr1dev/Golang-Blockchain.git/blockchain"
 )
 
 func main() {
-	chain := InitBlockChain()
+	chain := blockchain.InitBlockChain()
 
 	chain.AddBlock("First Block after Genesis")
 	chain.AddBlock("Second Block after Genesis")
 	chain.AddBlock("Third Block after Genesis")
 
-	for _, block := range chain.blocks {
-		fmt.Printf("\n")
-		fmt.Printf("Previus Hash: %x\n", block.PrevHash)
+	for _, block := range chain.Blocks {
+
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
 	}
 }
